a2efa5f7936655d7cfa5d63416325e4d8d31dcd2
sidman1996 <sidmanjhi1997@gmail.com>
2024-09-09 02:15:55 +0530

    add the file

---
+++ src/Sept/ex_05092024/class001.java
@@ -0,0 +1,10 @@
+package Sept.ex_05092024;
+
+public class class001 {
+    public static void main(String[] args) {
+        System.out.println("my name is siddhant");
+        System.out.println(2+2);
+        System.out.println(2*16);
+
+    }
+}
