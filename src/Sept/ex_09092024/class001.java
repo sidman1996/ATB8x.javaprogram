ab00a6cc6221f1c6842d8bd9172ff67a9c961fda
sidman1996 <sidmanjhi1997@gmail.com>
2024-09-11 01:02:09 +0530

    add the file

---
+++ src/Sept/ex_09092024/class001.java
@@ -0,0 +1,18 @@
+package Sept.ex_09092024;
+
+public class class001 {
+    public static void main(String[] rough){
+        System.out.println( "will try to comment");
+    }
+}
+// here is one line comment
+/*
+ * and this is multiline comment
+ *
+ *
+ */
+
+
+
+
+
