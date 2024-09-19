63109a37c8a2be14b715711d2da7f2402381de63
sidman1996 <sidmanjhi1997@gmail.com>
2024-09-20 03:13:13 +0530

    add the file

---
+++ src/Sept/ex_13092024/class001.java
@@ -0,0 +1,8 @@
+package Sept.ex_13092024;
+
+public class class001 {
+    public static void main(String[] args) {
+        System.out.println("hello ");
+        System.out.println("what are you doing ");
+    }
+}
