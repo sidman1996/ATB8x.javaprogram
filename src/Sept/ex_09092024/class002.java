eff20409ac6fea80332dd288bf35dc58020a5685
sidman1996 <sidmanjhi1997@gmail.com>
2024-09-19 19:50:54 +0530

    add the file

---
+++ src/Sept/ex_09092024/class002.java
@@ -0,0 +1,8 @@
+package Sept.ex_09092024;
+
+public class class002 {
+    public static void main(String[] anything) {
+        boolean sid_ = true;
+        boolean is_sun_rise_from_east= true;
+    }
+}
