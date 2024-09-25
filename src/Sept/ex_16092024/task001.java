3bd93423792d3381268ea1cf5e514c8ab4bf4d41
sidman1996 <sidmanjhi1997@gmail.com>
2024-09-26 02:08:44 +0530

    add the files

---
+++ src/Sept/ex_16092024/task001.java
@@ -0,0 +1,17 @@
+package Sept.ex_16092024;
+
+public class task001 {
+    public static void main(String[] args) {
+
+
+
+        short s = 10;
+
+        char c = 'A'; //65
+
+        int ss = s+c;
+        System.out.println(ss);
+
+
+    }
+}
